9420d23d883fe99d2b62fb64b44031faae3ac409
pjm349 <pjm349@drexel.edu>
2019-03-04 17:38:04 -0500

    Updated killall.sh

--- killall.sh
+++ killall.sh
@@ -1,38 +1,24 @@
 #!/bin/bash
 
-rm -f clean_out_2
+rm -f clean_out_2 
 
-if [ -f $1 ]
-then
-    ./get_table_headers.sh $1 
-else
-    echo "Unable to find file " $1
-fi
-
-count=0
+start_copy_line=1
 start_line=1
 current_line=1
 
 touch clean_out_2
 
-# extract out all the stuff in-between. Once you get to an END:VEVENT, you need to work your magic
-# with the table headers
 while read line; do
-    if [[ "$line" == "BEGIN:VEVENT" ]]
+    if [[ "$line" == "BEGIN:VALARM" ]]
     then
-        start_line=$((current_line+1))
-    elif [[ "$line" == "END:VEVENT" ]]
+        start_line=$((current_line))
+    elif [[ "$line" == "END:VALARM" ]]
     then
-        # Okay, now you need to pad the empty fields with whatever you just chopped.
-        echo "BEGIN:VEVENT" >> clean_out_2
-        echo "sed -n $start_line, $((current_line-1)) p $1"
-
-        echo "$(sed -n "$start_line,$((current_line-1)) p" $1)" > clean_out_temporary_2
-        ./pad_empty_fields.sh clean_out_temporary_2 >> clean_out_2
-
-        echo "END:VEVENT" >> clean_out_2
+	echo "$(sed -n "$start_copy_line, $((start_line-1)) p" $1)" >> clean_out_2
+	start_copy_line=$((current_line+1))
     fi
     current_line=$((current_line+1))
 done < $1
 
-#cat clean_out
+line_num=$(cat $1 | wc -l)
+echo "$(sed -n "$start_copy_line, $line_num p" $1)" >> clean_out_2

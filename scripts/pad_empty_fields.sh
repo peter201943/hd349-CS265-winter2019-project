77b3f8b77a62fa3aea19ffb3de1c25122bf95bbd
Henry Dang <henrydangprg@gmail.com>
2019-03-18 12:39:35 -0400

    Fix documentation for everything

--- scripts/pad_empty_fields.sh
+++ scripts/pad_empty_fields.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-#pad_empty_fields.sh
-#Pads out any missing fields in the headers list by setting that missing field to a blank string.  This script is called repeatedly for every VEVENT in an ics file. For example, not all VEVENTs have a URL field, but if even a single VEVENT has a URL field, then all of them will have the URL field set to an empty string. This is done to make it easier to generate the HTML table.
+# pad_empty_fields.sh Pads out any missing fields in the headers list by
+# setting that missing field to a blank string.  This script is called repeatedly
+# for every VEVENT in an ics file. For example, not all VEVENTs have a URL field,
+# but if even a single VEVENT has a URL field, then all of them will have the
+# URL field set to an empty string. This is done to make it easier to
+# generate the HTML table.
 
 path=$(dirname $0)
 

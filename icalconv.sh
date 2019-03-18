3e09d51c9226809983c650532c01cc2178399229
Henry Dang <henrydangprg@gmail.com>
2019-03-18 11:34:54 -0400

    Fix bug in icalconv.sh

--- icalconv.sh
+++ icalconv.sh
@@ -5,14 +5,14 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+filename=$1
 if [[ `wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
     wget $1
     filename=${$1##*/}
-    1="$filename"
 fi
 
 if [[ -f $1 && $1 =~ \.ics$ ]]; then
-    make run FILE=$1
+    make run FILE=$filename
 else
     echo "That file either does not exist, or it does not have an .ics extension"
 fi
